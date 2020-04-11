35e78d864863f015e7a599f47f6ff7392cb0fb8b
FJJI <fjjimenez@miuandes.cl>
2020-04-11 17:18:58 -0400

    validations done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,15 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
 class Customer < ApplicationRecord
   has_many :orders
   has_many :tickets, through: :orders
+  validates :email, presence: true, email: true
+  validates :email, uniqueness: true
+  validates :name, allow_blank: false
+  validates :lastname, allow_blank: false
 end
--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,3 +1,9 @@
 class EventVenue < ApplicationRecord
   has_many :events
+  validates :name, presence: { strict: true }
+  validates :address, presence: { strict: true }
+  validates :capacity, presence: { strict: true }, numericality: { only_integer: true, greater_than_or_equal_to:10 }
 end
+
+#The name and address of an EventVenue must not be blank. In addition, capacity
+# must be integer, with 10 as the minimum value.
