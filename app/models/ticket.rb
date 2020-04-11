98b4ee76b8977e0654c39e1b15be16015248c666
FJJI <fjjimenez@miuandes.cl>
2020-04-11 17:43:53 -0400

    primer callback

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,12 +2,19 @@ class Ticket < ApplicationRecord
   belongs_to :order
   belongs_to :ticket_type
 
-  # TODO: complete the folowing
-  # before_xxxxx :update_stats
+  after_save :update_stats
+  before_destroy :destoy_ticket
 
   private
     def update_stats
       es = self.ticket_type.event.event_stat
-      # TODO: complete in order to update event stats
+      es.tickets_sold += 1
+      es.attendance += 1
     end
+  private
+  def destoy_ticket
+    es = self.ticket_type.event.event_stat
+    es.tickets_sold -= 1
+    es.attendance -= 1
+  end
 end
